ea03d51eb273c4395a6983045fa323695b49642a
Gerson Espinal <gerson.espinal@gmail.com>
2020-05-15 16:06:45 -0400

    - Reverse String - Two Sum II

--- leetcode/reverseString.js
+++ leetcode/reverseString.js
@@ -3,11 +3,21 @@
 // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
 const reverseString = (s) => {
-    return s.reverse();
+  return reverseFunc(s, 0, s.length - 1);
 };
 
-const arr1 = ["h","e","l","l","o"];
-const arr2 = ["H","a","n","n","a","h"];
+const reverseFunc = (arr, start, end) => {
+  if (start >= end) {
+    return arr;
+  }
 
-console.log(reverseString(arr1))
-console.log(reverseString(arr2))
+  [arr[start], arr[end]] = [arr[end], arr[start]];
+
+  return reverseFunc(arr, start + 1, end - 1);
+};
+
+const arr1 = ["h", "e", "l", "l", "o"];
+const arr2 = ["H", "a", "n", "n", "a", "h"];
+
+console.log(reverseString(arr1));
+console.log(reverseString(arr2));
