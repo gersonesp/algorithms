0124e0ddc33da9949a25bddda9f55f43e63515bf
Gerson Espinal <gerson.espinal@gmail.com>
2020-05-27 10:29:43 -0400

    Valid Palindrome Updated

--- leetcode/validPalindrome.js
+++ leetcode/validPalindrome.js
@@ -3,16 +3,12 @@
 // Note: For the purpose of this problem, we define empty string as valid palindrome.
 
 const isPalindrome = (s) => {
-  const regex = /[a-z0-9]/i;
-  let i = 0;
-  let j = s.length - 1;
+  const regex = /[^A-Za-z0-9]/g;
+  const str = s.toLowerCase().replace(regex, "");
+  const len = str.length;
 
-  while (i <= j) {
-    if (!regex.test(s[i])) {
-      i++;
-    } else if (!regex.test(s[j])) {
-      j--;
-    } else if (s[i++].toLowerCase() !== s[j--].toLowerCase()) {
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
       return false;
     }
   }
